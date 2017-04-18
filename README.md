f0a329f09d837afa73374d17c1445200ec24464d
Allen.Zou <yalong.zou@hyku.com>
2017-04-18 23:30:07 +0800

    set sql callable; support `placeholder` for sql

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# sql-builder
-A tool to build sql statement, avoiding to write SQLs directly. 
+# sql()-builder
+A tool to build sql() statement, avoiding to write SQLs directly. 
 
 
 ## Install
 ```sh
-pip install sql-builder
+pip install sql()-builder
 ```
 
 ## Examples
@@ -16,7 +16,7 @@ pip install sql-builder
 #    student_snapshot: id, name, class_id, age
 #    teacher: id, name
 #    teach: teacher_id(teacher:id), class_id(class:id)
-import sql_builder
+import sql()_builder
 
 
 student = Table("student").as_("s")
@@ -25,39 +25,39 @@ class_ = Table("class").as_("c")
 teacher = Table("teacher")
 teach = Table("teach").as_("ss")
 
-student.select(student.builtin_all, student.age.max_()).sql
+student.select(student.builtin_all, student.age.max_()).sql()
 # ('SELECT `s`.*, MAX(`s`.`age`) FROM `student` AS `s`', [])
 
-student.select(student.builtin_all, student.age.max_("max_age"))[0:4].sql
+student.select(student.builtin_all, student.age.max_("max_age"))[0:4].sql()
 # ('SELECT `s`.*, MAX(`s`.`age`) AS `max_age` FROM `student` AS `s` LIMIT 0, 4', [])
 
-student.join(class_, student.class_id == class_.id).select().sql
+student.join(class_, student.class_id == class_.id).select().sql()
 # ('SELECT * FROM `student` AS `s` INNER JOIN `class` AS `c` ON `s`.`class_id` = `c`.`id`', [])
 
-teacher.join(teach, teach.teacher_id == teacher.id).join(class_, class_.id == teach.class_id).select(teacher.builtin_all).where(class_.id == '12321').sql
+teacher.join(teach, teach.teacher_id == teacher.id).join(class_, class_.id == teach.class_id).select(teacher.builtin_all).where(class_.id == '12321').sql()
 # ('SELECT `teacher`.* FROM `teacher` INNER JOIN `teach` AS `ss` ON `ss`.`teacher_id` = `teacher`.`id` INNER JOIN `class` AS `c` ON `c`.`id` = `ss`.`class_id` WHERE `c`.`id` = %s', ['123123'])
 
-teacher.join(teach, teach.teacher_id == teacher.id).join(class_, class_.id == teach.class_id).select(teacher.builtin_all).where(class_.id == '123123').sql
+teacher.join(teach, teach.teacher_id == teacher.id).join(class_, class_.id == teach.class_id).select(teacher.builtin_all).where(class_.id == '123123').sql()
 # ('SELECT `teacher`.* FROM `teacher` INNER JOIN `teach` AS `ss` ON `ss`.`teacher_id` = `teacher`.`id` INNER JOIN `class` AS `c` ON `c`.`id` = `ss`.`class_id` WHERE `c`.`id` = %s', ['123123'])
 
 
-student.insert(student.id, 1, student.name, "学生a", student.class_id, "21321").on_duplicate_key_fields(student.name, "学生a").sql
+student.insert(student.id, 1, student.name, "学生a", student.class_id, "21321").on_duplicate_key_fields(student.name, "学生a").sql()
 # ('INSERT INTO `student`(id, name, class_id) VALUES(%s, %s, %s) ON DUPLICATE KEY UPDATE `name`=%s', [1, '学生a', '21321', '学生a'])
 
 sub = student.select(student.id, student.name, student.class_id, student.age).where(student.name == 'test').as_table("old_student")
 
-InsertFromSelect(ss, [ss.id, ss.name, ss.class_id, ss.age], sub).sql
+InsertFromSelect(ss, [ss.id, ss.name, ss.class_id, ss.age], sub).sql()
 # ('INSERT INTO `student_snapshot`(`snapshot`.`id`, `snapshot`.`name`, `snapshot`.`class_id`, `snapshot`.`age`) (SELECT `s`.`id`, `s`.`name`, `s`.`class_id`, `s`.`age` FROM `student` AS `s` WHERE `s`.`name` = %s) AS `old_student`', ['test'])
 
 
-student.update(student.name, "学生").where(student.id == 1).sql
+student.update(student.name, "学生").where(student.id == 1).sql()
 # ('UPDATE `student` SET `s`.`name`=%s WHERE `s`.`id` = %s', ['学生', 1])
 
 
-student.delete().where((student.name == 'allen')&(student.id != '1231')).sql
+student.delete().where((student.name == 'allen')&(student.id != '1231')).sql()
 # ('DELETE FROM `student` AS `s` WHERE `s`.`name` = %s AND `s`.`id` != %s',['allen', '1231'])
 
-teacher.delete().where(teacher.id.in_(Select(tables=teach.join(teacher, teach.teacher_id == teacher.id)).select(teacher.id).where((teach.class_id == 2) & (teacher.deleted == 0)))).sql
+teacher.delete().where(teacher.id.in_(Select(tables=teach.join(teacher, teach.teacher_id == teacher.id)).select(teacher.id).where((teach.class_id == 2) & (teacher.deleted == 0)))).sql()
 # ('DELETE FROM `teacher` WHERE `teacher`.`id` IN (SELECT `teacher`.`id` FROM `teach` AS `ss` INNER JOIN `teacher` ON `ss`.`teacher_id` = `teacher`.`id` WHERE `ss`.`class_id` = %s AND `teacher`.`deleted` = %s)', [2, 0])
 
 ```
--- sql_builder/sql.py
+++ sql_builder/sql.py
@@ -164,8 +164,7 @@ class _Table(object):
     def field_view(self):
         raise NotImplemented
 
-    @property
-    def from_view(self):
+    def from_view(self, placeholder="%s"):
         raise NotImplemented
 
     @property
@@ -191,9 +190,8 @@ class _SubQueryTable(_Table):
         self._alias = alias
         self._query = query
 
-    @property
-    def from_view(self):
-        sub_query_sql, sub_query_args = self._query.sql
+    def from_view(self, placeholder="%s"):
+        sub_query_sql, sub_query_args = self._query.sql(placeholder)
         return "({}) AS `{}`".format(sub_query_sql, self._alias), sub_query_args
 
     @property
@@ -247,8 +245,7 @@ class Table(_Table):
             return "`{}`".format(self._b_alias)
         return self.raw_view
 
-    @property
-    def from_view(self):
+    def from_view(self, placeholder="%s"):
         if self._b_alias:
             return "{} AS `{}`".format(self.raw_view, self._b_alias), []
         return self.raw_view, []
@@ -311,16 +308,15 @@ class TableJoin(_Table):
     def outer_join(self, table, condition):
         return self.join(table, condition, TableJoin.OUTER_JOIN)
 
-    @property
-    def from_view(self):
+    def from_view(self, placeholder="%s"):
         args = []
-        base_sql, base_args = self.base.from_view
+        base_sql, base_args = self.base.from_view(placeholder)
         pieces = [base_sql]
         args.extend(base_args)
         for each in self.join_items:
-            tbl_sql, tbl_args = each.table.from_view
+            tbl_sql, tbl_args = each.table.from_view(placeholder)
             args.extend(tbl_args)
-            cond_sql, cond_args = each.condition.sql
+            cond_sql, cond_args = each.condition.sql(placeholder)
             args.extend(cond_args)
             pieces.append("{} {} ON {}".format(each.method, tbl_sql, cond_sql))
         return " ".join(pieces), args
@@ -336,8 +332,7 @@ class _Where(object):
     def __invert__(self):
         pass
 
-    @property
-    def sql(self):
+    def sql(self, placeholder="%s"):
         return
 
 
@@ -417,19 +412,18 @@ class Condition(_Where):
         else:
             raise ValueError()
 
-    @property
-    def sql(self):
+    def sql(self, placeholder="%s"):
         key = self.column.where_view
         op = self._op_2_sql(self.op)
         # sql_pieces = [self.column.where_view, self._op_2_sql(self.op)]
         args = []
-        sub_sql, sub_args = self.value.sql if isinstance(self.value, Select) else ("", [])
+        sub_sql, sub_args = self.value.sql(placeholder) if isinstance(self.value, Select) else ("", [])
         if self.op in [Condition.OP_IN, Condition.OP_NIN]:
             if isinstance(self.value, Select):
                 value = "({})".format(sub_sql)
                 args.extend(sub_args)
             else:
-                value = "({})".format(",".join("%s" for _ in self.value))
+                value = "({})".format(",".join(placeholder for _ in self.value))
                 args.extend(self.value)
         elif self.op in [Condition.OP_LIKE, Condition.OP_NOT_LIKE]:
             value = "%%{}%%".format(self.value)
@@ -447,7 +441,7 @@ class Condition(_Where):
                 value = "({})".format(sub_sql)
                 args.extend(sub_args)
             else:
-                value = "%s"
+                value = placeholder
                 args.append(self.value)
         else:
             if isinstance(self.value, Column):
@@ -456,7 +450,7 @@ class Condition(_Where):
                 value = "({})".format(sub_sql)
                 args.extend(sub_args)
             else:
-                value = "%s"
+                value = placeholder
                 args.append(self.value)
         return " ".join([key, op, value]), args
 
@@ -494,11 +488,10 @@ class ConditionUnion(_Where):
         else:
             raise ValueError()
 
-    @property
-    def sql(self):
+    def sql(self, placeholder="%s"):
         args = []
-        left_sql, left_args = self.left.sql
-        right_sql, right_args = self.right.sql
+        left_sql, left_args = self.left.sql(placeholder)
+        right_sql, right_args = self.right.sql(placeholder)
         args.extend(left_args)
         args.extend(right_args)
         left_sql = ("{}" if isinstance(self.left, Condition) else "({})").format(left_sql)
@@ -510,7 +503,7 @@ class ConditionUnion(_Where):
         return tpl.format(left_sql, right_sql), args
 
     def __str__(self):
-        return "{}: {} {}".format(super(ConditionUnion, self).__str__(), *self.sql)
+        return "{}: {} {}".format(super(ConditionUnion, self).__str__(), *self.sql())
 
 
 class Sort(object):
@@ -576,18 +569,17 @@ class Insert(_Query):
             self._on_duplicate_update_fields.append(_Query.UpdatePair(key, val))
         return self
 
-    @property
-    def sql(self):
+    def sql(self, placeholder="%s"):
         sql_pieces = ["INSERT INTO {table}({fields}) VALUES({placeholders})".format(table=self._tables.raw_view,
                                                                                     fields=", ".join(
                                                                                         pair.field.name for pair
                                                                                         in self._pairs),
                                                                                     placeholders=", ".join(
-                                                                                        ["%s"] * len(self._pairs)))]
+                                                                                        [placeholder] * len(self._pairs)))]
         args = [pair.value for pair in self._pairs]
         if self._on_duplicate_update_fields:
             sql_pieces.append("ON DUPLICATE KEY UPDATE {}".format(
-                ", ".join("{}=%s".format(pair.field.insert_view) for pair in self._on_duplicate_update_fields)))
+                ", ".join("{}={}".format(pair.field.insert_view, placeholder) for pair in self._on_duplicate_update_fields)))
             args.extend([pair.value for pair in self._on_duplicate_update_fields])
         return " ".join(sql_pieces), args
 
@@ -618,9 +610,8 @@ class InsertFromSelect(_Query):
             self._on_duplicate_update_fields.append(_Query.UpdatePair(key, val))
         return self
 
-    @property
-    def sql(self):
-        sub_query_sql, sub_query_args = self._sub_query.from_view
+    def sql(self, placeholder="%s"):
+        sub_query_sql, sub_query_args = self._sub_query.from_view(placeholder)
         sql_pieces = ["INSERT INTO {table}({fields}) {sub_query}".format(table=self._tables.raw_view,
                                                                          fields=", ".join(field.raw_view for field in self._fields),
                                                                          sub_query=sub_query_sql)]
@@ -647,15 +638,14 @@ class Update(_Query):
         self._where = cond
         return self
 
-    @property
-    def sql(self):
+    def sql(self, placeholder="%s"):
         sql_pieces = ["UPDATE {table} SET {fields}".format(table=self._tables.raw_view,
                                                            fields=", ".join(
-                                                               "{}=%s".format(pair.field.update_view) for pair in
+                                                               "{}={}".format(pair.field.update_view, placeholder) for pair in
                                                                self._pairs))]
         args = [pair.value for pair in self._pairs]
         if self._where:
-            where_clause, where_args = self._where.sql
+            where_clause, where_args = self._where.sql(placeholder)
             sql_pieces.append("WHERE {}".format(where_clause))
             args.extend(where_args)
         return " ".join(sql_pieces), args
@@ -732,17 +722,16 @@ class Select(_Query):
     def as_table(self, alias):
         return _SubQueryTable(alias, self)
 
-    @property
-    def sql(self):
+    def sql(self, placeholder="%s"):
         sql_pieces = []
         args = []
         fields = self._fields and ", ".join(
             field.field_view for field in self._fields) or "*"
-        from_sql, from_args = self._tables.from_view
+        from_sql, from_args = self._tables.from_view(placeholder)
         sql_pieces.append("SELECT {fields} FROM {tables}".format(fields=fields, tables=from_sql))
         args.extend(from_args)
         if self._where:
-            where_clause, where_args = self._where.sql
+            where_clause, where_args = self._where.sql(placeholder)
             sql_pieces.append("WHERE {where}".format(where=where_clause))
             args.extend(where_args)
         if self._sort:
@@ -765,14 +754,13 @@ class Delete(_Query):
         self._where = cond
         return self
 
-    @property
-    def sql(self):
+    def sql(self, placeholder="%s"):
         args = []
-        from_sql, from_args = self._tables.from_view
+        from_sql, from_args = self._tables.from_view(placeholder)
         args.extend(from_args)
         sql_pieces = ["DELETE FROM {table}".format(table=from_sql)]
         if self._where:
-            where_clause, where_args = self._where.sql
+            where_clause, where_args = self._where.sql(placeholder)
             sql_pieces.append("WHERE {}".format(where_clause))
             args.extend(where_args)
         return " ".join(sql_pieces), args
@@ -792,26 +780,26 @@ if __name__ == "__main__":
     class_ = Table("class").as_("c")
     teacher = Table("teacher")
     teach = Table("teach").as_("ss")
-    print(Select(tables=student, fields=[student.builtin_all, student.age.max_()]).sql)
-    print(Select(tables=student).select(student.builtin_all, student.age.max_("max_age"))[0:4].sql)
-    print(student.select().sql)
+    print(Select(tables=student, fields=[student.builtin_all, student.age.max_()]).sql())
+    print(Select(tables=student).select(student.builtin_all, student.age.max_("max_age"))[0:4].sql())
+    print(student.select().sql())
 
-    print(Select(tables=student.join(class_, student.class_id == class_.id)).asc(class_.name).sql)
+    print(Select(tables=student.join(class_, (student.class_id == class_.id) & (student.age == 20))).asc(class_.name).sql("?"))
     print(Select(tables=teacher.join(teach,
                                      teach.teacher_id == teacher.id).join(class_, class_.id == teach.class_id),
-                 where=(class_.id == '123123'), fields=[teacher.builtin_all]).sql)
+                 where=(class_.id == '123123'), fields=[teacher.builtin_all]).sql())
     print(Select(tables=teacher.join(teach,
                                      teach.teacher_id == teacher.id).join(class_, class_.id == teach.class_id)).where(
-        class_.id == None).select(teacher.builtin_all).sql)
+        class_.id == None).select(teacher.builtin_all).sql())
     print("=" * 20)
     print(Insert(student, student.id, 1, student.name, "学生a", student.class_id, "21321").on_duplicate_key_fields(
-        student.name, "学生a").sql)
+        student.name, "学生a").sql())
 
     sub = Select(student).where(student.name == 'test').select(
         student.id, student.name, student.class_id, student.age).as_table("old_student")
-    print(InsertFromSelect(ss, [ss.id, ss.name, ss.class_id, ss.age], sub).sql)
+    print(InsertFromSelect(ss, [ss.id, ss.name, ss.class_id, ss.age], sub).sql())
 
-    print(Update(student, student.name, "学生").where(student.id == 1).sql)
-    print(Delete(table=student).where(student.id == 1).sql)
+    print(Update(student, student.name, "学生").where(student.id == 1).sql())
+    print(Delete(table=student).where(student.id == 1).sql())
     print(Delete(table=teacher).where(teacher.id.in_(
-        Select(tables=teach.join(teacher, teach.teacher_id == teacher.id)).select(teacher.id).where((teach.class_id == 2) & (teacher.deleted == 0)))).sql)
+        Select(tables=teach.join(teacher, teach.teacher_id == teacher.id)).select(teacher.id).where((teach.class_id == 2) & (teacher.deleted == 0)))).sql())
